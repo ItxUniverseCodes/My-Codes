b342411d0f27c008295881e6f67a889db0fc23dc
Kainat Nadeem <bc220422631kna@vu.edu.pk>
2026-02-25 23:32:14 +0500

    Create Build a Report Card Printer.py

---
+++ Build a Report Card Printer.py
@@ -0,0 +1,14 @@
+name = 'Alice'
+#Printing the name and data type of name 
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+#Checking if the score variable is int by using isinstance()
+print(isinstance(score, float))
+print(score, type(score))
